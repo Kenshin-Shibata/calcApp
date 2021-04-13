faa951370766e26e42e5cb72ed89e0d7a218e93d
kenshin-Shibata <ps0139hk@gmail.com>
2021-04-13 23:05:11 +0900

    firstCommit

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = calcCollectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
 
+    @IBOutlet weak var calcHeightConstrait: NSLayoutConstraint!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var calcCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        calcCollectionView.delegate = self
+        calcCollectionView.dataSource = self
+        calcCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
     }
 
 
