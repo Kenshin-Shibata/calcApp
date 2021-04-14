993a078450fe66fb31412c16bf98ec72ef066968
kenshin-Shibata <ps0139hk@gmail.com>
2021-04-15 01:05:17 +0900

    saisyo

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -21,12 +21,18 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = (collectionView.frame.width - 3 * 10) / 4
-        return .init(width: width, height: width)
+        var width:CGFloat = 0
+        width = ((collectionView.frame.width - 10) - 14 * 5 ) / 4
+        let height = width
+        if indexPath.section == 4 && indexPath.row == 0 {
+            width = width * 2 + 14 + 9
+        }
+        
+        return .init(width: width, height: height)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
+        return 14
     }
     
     //こちらでボタンのスタイル等を変えていく
@@ -34,11 +40,66 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         let cell = calcCollectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! CalcViewCell
         cell.numberLabel.text = numbers[indexPath.section][indexPath.row]
         
+        numbers[indexPath.section][indexPath.row].forEach { (numberString) in
+            if "0"..."9" ~= numberString || numberString.description == "." {
+                cell.numberLabel.backgroundColor = .darkGray
+            } else if numberString == "C" || numberString == "%" || numberString == "$" {
+                cell.numberLabel.backgroundColor = UIColor.init(white: 1, alpha: 0.7)
+                cell.numberLabel.textColor = .black
+            }
+        }
+        
+        
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let number = numbers[indexPath.section][indexPath.row]
+        if calcStatus == .none {
+            switch number {
+            case "0"..."9":
+                numberLabel.text = number
+            case "+":
+                firstNumber = numberLabel.text ?? ""
+                calcStatus = .plus
+            case "C":
+                clear()
+            default:
+               break
+            }
+        } else if calcStatus == .plus {
+            switch number {
+            case "0"..."9":
+                numberLabel.text = number
+            case "=":
+                secondNumber = numberLabel.text ?? ""
+                
+                let firstNum = Double(firstNumber) ?? 0
+                let secondNum = Double(secondNumber) ?? 0
+                
+                numberLabel.text = String(firstNum + secondNum)
+            case "C":
+                clear()
+            default:
+                break
+            }
+        }
+    }
+    
+    func clear(){
+        numberLabel.text = "0"
+        calcStatus = .none
+    }
+    enum CalcStatus {
+        case none, plus
+    }
+    
+    var firstNumber = ""
+    var secondNumber = ""
+    var calcStatus: CalcStatus = .none
+    
     let numbers = [
-        ["C","%","S","+"],
+        ["C","%","$","+"],
         ["7","8","9","×"],
         ["4","5","6","-"],
         ["1","2","3","+"],
@@ -57,6 +118,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         calcCollectionView.register(CalcViewCell.self, forCellWithReuseIdentifier: "cellId")
         calcHeightConstrait.constant = view.frame.width * 1.4
         calcCollectionView.backgroundColor = .clear
+        calcCollectionView.contentInset = .init(top: 0, left: 14, bottom: 0, right: 14)
         
         view.backgroundColor = .black
     }
